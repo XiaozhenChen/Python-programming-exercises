b2b8889e48a6576ed0b28a1d0e06bbec61ffa7ab
unknown <skycxz@qq.com>
2019-04-26 11:01:47 +0800

    input split ,

--- test.py
+++ test.py
@@ -12,6 +12,22 @@ class testClass(object):
     def printString(self):
         print(self.Str)
 
-obj = testClass()
-obj.getString()
-obj.printString()
+
+s = input("请输入一行非负整数，以空格隔开，-1结束:")
+# s = '1 67 56 78 -1'
+list1 = s.split(',')
+list2 = []
+list3 = []
+
+for line in list1:
+    list2.append(int(line))
+
+import math
+c=50
+h=30
+
+for line in list2:
+   
+    list3.append(str(int(round(math.sqrt(2*c*float(line)/h))))) 
+
+print( ','.join(list3))
