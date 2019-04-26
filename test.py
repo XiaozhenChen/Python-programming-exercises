cf8ef23c4bd7996c9a08d481f3e4ae3e30c188bb
unknown <skycxz@qq.com>
2019-04-26 10:29:20 +0800

    class method _init_

---
+++ test.py
@@ -0,0 +1,17 @@
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x-1)
+
+
+class testClass(object):
+    def __init__(self):#进行初始化
+        self.Str = ""
+    def getString(self):
+        self.Str= input() #python3 input, python2 raw_input
+    def printString(self):
+        print(self.Str)
+
+obj = testClass()
+obj.getString()
+obj.printString()
