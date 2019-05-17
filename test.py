6e9b55af947fff7a21e57c3dfd7a591fc98475f5
XiaozhenChen <skycxz@qq.com>
2019-05-17 11:12:07 +0800

    list,tuple,class,function

--- test.py
+++ test.py
@@ -13,7 +13,75 @@ class testClass(object):
         print(self.Str)
 
 
+def joinStr():
+    s = input()
+    words = [word for word in s.split(" ")]
+    print(" ".join(sorted(list(set(words)))))
 
-s = input()
-words = [word for word in s.split(" ")]
-print(" ".join(sorted(list(set(words)))))
+def addString():
+
+    a = input()
+    n1 = int( "%s" % a )
+    n2 = int( "%s%s" % (a,a) )
+    n3 = int( "%s%s%s" % (a,a,a) )
+    n4 = int( "%s%s%s%s" % (a,a,a,a) )
+    print(n1+n2+n3+n4)
+
+def dictFrequency():
+    f = {}
+    l = input()
+    for w in l.split():
+        f[w] = f.get(w,0) +1
+    words = f.keys()
+    words.sort()
+
+    for w in words:
+        print("%s:%d" % (w,f[w]))
+
+def square(num):
+    '''
+       Return the square
+    '''
+    return num**2
+
+
+class Person:
+    name = "Person"
+    
+    def __init__(self, name = None):
+        self.name = name
+def SumFunction(num1, num2):
+    return num1 + num2
+
+def printValue(s1,s2):
+	print(s1+s2)
+
+def printDict():
+    d = dict()
+    for i in range(1,21):
+        d[i] = i**2
+    print(d)
+    for (k,v) in d.items():
+        print(v)
+def printList():
+    li = list()
+    for i in range(1,21):
+        li.append(i**2)
+    print(li[-5:])
+
+
+def printTuple():
+    li=list()
+    for i in range(1,21):
+        li.append(i**2)
+    print(tuple(li))
+
+
+tp=(1,2,3,4,5,6,7,8,9,10)
+li=list()
+for i in tp:
+    if tp[i-1]%2==0:
+        li.append(tp[i-1])
+tp2=tuple(li)
+print(tp2)
+print(tuple.__doc__)
